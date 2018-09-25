370a8f2f3b457bb15a1d67c22e1086dbc00773aa
petrsnd <petrsnd@gmail.com>
2018-09-24 22:41:58 -0600

    Listener needs dispose. So do many other classes

--- SafeguardDotNet/SafeguardEventListener.cs
+++ SafeguardDotNet/SafeguardEventListener.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace OneIdentity.SafeguardDotNet
 {
     internal class SafeguardEventListener : ISafeguardEventListener
     {
-        public SafeguardEventListener()
+        bool _disposed;
+
+        string _eventUrl;
+        SecureString _apiToken;
+        X509Certificate2 _certificate;
+
+        HubConnection _signalrConnection;
+
+        public SafeguardEventListener(string baseUrl, SecureString apiToken)
         {
+            _apiToken = apiToken;
+
+        }
+
+        public SafeguardEventListener(string baseUrl, X509Certificate2 certificate)
+        {
+            _certificate = certificate;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (!_disposed)
+            {
+                _apiToken?.Dispose();
+                try
+                {
+                    _signalrConnection?.Dispose();
+                }
+                finally
+                {
+                    _signalrConnection = null;
+                }
+                _disposed = true;
+            }
         }
 
         public void RegisterEventHandler(string eventName, SafeguardEventHandler handler)
