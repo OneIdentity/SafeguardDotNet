e980fdfe0c95eae62914e8925b1d1dace2ab7919
petrsnd <petrsnd@gmail.com>
2021-03-24 11:14:56 -0600

    version info wasn't good

--- versionnumber.ps1
+++ versionnumber.ps1
@@ -5,7 +5,9 @@ Param(
     [Parameter(Mandatory=$true, Position=1)]
     [string]$SemanticVersion,
     [Parameter(Mandatory=$true, Position=2)]
-    [string]$BuildId
+    [string]$BuildId,
+    [Parameter(Mandatory=$true, Position=3)]
+    [bool]$IsPrerelease
 )
 
 Write-Host "SemanticVersion = $SemanticVersion"
@@ -14,20 +16,33 @@ Write-Host "BuildId = $BuildId"
 $local:BuildNumber = ($BuildId - 105000)
 Write-Host "BuildNumber = $($local:BuildNumber)"
 
-$local:VersionString = "${SemanticVersion}.$($local:BuildNumber)"
-$local:TemplateVersion = "9999.9999.9999.9999"
-Write-Host "VersionString = $($local:VersionString)"
-Write-Host "TemplateVersion = $($local:TemplateVersion)"
-
-Write-Host "Searching for files with version info in '$SourceDir'"
-(Get-ChildItem -Recurse -Include @("AssemblyInfo.cs","*.csproj")) | Where-Object { (Get-Content -Raw $_.Fullname) -like "*$($local:TemplateVersion)*" } | ForEach-Object {
-    $local:Path = $_.FullName
-    Write-Host "Replacing version information in '$($local:Path)'"
-    (Get-Content $local:Path -Raw).replace($local:TemplateVersion, $local:VersionString) | Set-Content $local:Path
-
-    Write-Output "*****"
-    Get-Content $local:Path
-    Write-Output "*****"
+$local:PackageCodeMarker = "9999.9999.9999"
+$local:AssemblyCodeMarker = "9999.9999.9999.9999"
+$local:AssemblyVersion = "${SemanticVersion}.$($local:BuildNumber)"
+if ($IsPrerelease)
+{
+    $local:PackageVersion = "${SemanticVersion}-dev-$($local:BuildNumber)"
 }
+else
+{
+    $local:PackageVersion = $SemanticVersion
+}
+Write-Host "PackageCodeMarker = $($local:PackageCodeMarker)"
+Write-Host "AssemblyCodeMarker = $($local:AssemblyCodeMarker)"
+Write-Host "PackageVersion = $($local:PackageVersion)"
+Write-Host "AssemblyVersion = $($local:AssemblyVersion)"
 
-Write-Output "##vso[task.setvariable variable=VersionString;]$($local:VersionString)"
+Write-Host "Replacing markers in SafeguardDotNet"
+$local:ProjectFile = (Join-Path $PSScriptRoot "SafeguardDotNet\SafeguardDotNet.csproj")
+(Get-Content $local:ProjectFile -Raw).replace($local:AssemblyCodeMarker, $local:AssemblyVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+(Get-Content $local:ProjectFile -Raw).replace($local:PackageCodeMarker, $local:PackageVersion) | Set-Content -Encoding UTF8 $local:ProjectFile
+
+Write-Host "Replacing markers in SafeguardDotNet.GuiLogin"
+$local:GuiAssemblyInfoFile = (Join-Path $PSScriptRoot "SafeguardDotNet.GuiLogin\Properties\AssemblyInfo.cs")
+$local:GuiNuspec = (Join-Path $PSScriptRoot "SafeguardDotNet.GuiLogin\SafeguardDotNet.GuiLogin.nuspec")
+(Get-Content $local:GuiAssemblyInfoFile -Raw).replace($local:AssemblyCodeMarker, $local:AssemblyVersion) | Set-Content -Encoding UTF8 $local:GuiAssemblyInfoFile
+(Get-Content $local:GuiNuspec -Raw).replace($local:PackageCodeMarker, $local:PackageVersion) | Set-Content -Encoding UTF8 $local:GuiNuspec
+
+
+Write-Output "##vso[task.setvariable variable=AssemblyVersion;]$($local:AssemblyVersion)"
+Write-Output "##vso[task.setvariable variable=PackageVersion;]$($local:PackageVersion)"
