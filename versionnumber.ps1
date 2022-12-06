3205ed7b1a31e3bbdf780002877accc16e92e532
Jared Custer <jared.custer@oneidentity.com>
2022-12-05 17:54:19 -0700

    Update versionnumber.ps1
    changed the way we calculate build number to see if the builds will succeed

--- versionnumber.ps1
+++ versionnumber.ps1
@@ -13,7 +13,7 @@ Param(
 Write-Host "SemanticVersion = $SemanticVersion"
 Write-Host "BuildId = $BuildId"
 
-$local:BuildNumber = ($BuildId - 108000)
+$local:BuildNumber = ($BuildId - 122000)
 Write-Host "BuildNumber = $($local:BuildNumber)"
 
 $local:PackageCodeMarker = "9999.9999.9999"
